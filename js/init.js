49e7b98faeb1e543985025586c41a53f09abfe39
Long Tran <ltran@Longs-MacBook-Pro.local>
2015-07-31 16:31:43 -0700

    prevent destroy site on mobile

--- js/init.js
+++ js/init.js
@@ -1,4 +1,7 @@
 function initLVT() {
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+ 		$("footer").html("<p>\"It doesn't matter if the glass is half empty or half full; all that matters is you're the one pouring the water.\" - Mark Cuban | Long Tran &copy; 2015");
+	}
 	$("body").animate({opacity: 0});
 	$("body").animate({opacity: 1}, 1500);
 	$("#events").hide();
