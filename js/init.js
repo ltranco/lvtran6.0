d5307a22608997d51f6166e21ac355b4e76fbfdf
Long Tran <v.long128@gmail.com>
2015-08-04 22:31:14 -0700

    Added more animation

--- js/init.js
+++ js/init.js
@@ -2,30 +2,25 @@ function initLVT() {
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  		$("footer").html("<p>\"It doesn't matter if the glass is half empty or half full; all that matters is you're the one pouring the water.\" - Mark Cuban | Long Tran &copy; 2015");
 	}
-	
 	$("body").animate({opacity: 1}, 1500);
 	$(".headerCustom").hide();
-	$("#events").hide();
 	$(".container").hide();
+	$("#events").hide()
 	$("footer").hide();
-
-	$(".introView").delay(3000).slideUp('slow', function() {$(".headerCustom").show(200);});
-
+	$(".introText").delay(3000).fadeOut(700);
+	$(".introView").delay(3000).slideUp('slow', function() {
+		$(".headerCustom").fadeIn(700, function() {
+			$(".container").fadeIn(1000, function() {
+				$("footer").fadeIn();		
+			});
+		});
+	});
 	$("#eventButton").click(function() {
-		$("#portfolio").hide(400);
-		$("#events").show(400);		
+		$("#portfolio").fadeOut(200);
+		$("#events").fadeIn(200);		
 	});
-
 	$("#projButton").click(function() {
-		$("#portfolio").show(400);
-		$("#events").hide(400);		
+		$("#portfolio").fadeIn(200);
+		$("#events").fadeOut(200);		
 	});
-
-	introView();
-}
-
-function introView() {
-	
-	
-
 }
