d7c5908cdf25867d8abd53c4370e121f1219de34
Long Tran <ltran@Longs-MacBook-Pro.local>
2015-08-18 15:51:05 -0700

    new background color for portfolio to match the minimalistic design principle and adhere to the ff3232 theme

--- js/init.js
+++ js/init.js
@@ -2,7 +2,6 @@ function initLVT() {
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  		$("footer").html("<p>\"It doesn't matter if the glass is half empty or half full; all that matters is you're the one pouring the water.\" - Mark Cuban | Long Tran &copy; 2015");
 	}
-	$("body").animate({opacity: 1}, 1500);
 	$(".headerCustom").hide();
 	$(".container").hide();
 	$("#events").hide()
