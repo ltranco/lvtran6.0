dce324300da3b14d5726353b15841d257ea876dd
Long Tran <ltran@Longs-MacBook-Pro.local>
2015-08-19 13:28:55 -0700

    wait for all body html to load

--- js/init.js
+++ js/init.js
@@ -2,15 +2,12 @@ function initLVT() {
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  		$("footer").html("<p>\"It doesn't matter if the glass is half empty or half full; all that matters is you're the one pouring the water.\" - Mark Cuban | Long Tran &copy; 2015");
 	}
-	$(".introView").show();
-	$(".headerCustom").hide();
-	$(".container").hide();
-	$("#events").hide()
-	$("footer").hide();
+
 	$(".introText").delay(1500).fadeOut(700);
 	$(".introView").delay(1800).slideUp('slow', function() {
-		$("body").show();
+		$("introView").show();
 		$(".headerCustom").fadeIn(700, function() {
+			$("#portfolio").fadeIn();
 			$(".container").fadeIn(1000, function() {
 				$("footer").fadeIn();		
 			});
