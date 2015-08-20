6ca516d86f23fa8543f815790db54607da99665d
Long Tran <ltran@Longs-MacBook-Pro.local>
2015-08-20 11:38:35 -0700

    adding resume

--- js/init.js
+++ js/init.js
@@ -2,28 +2,31 @@ function initLVT() {
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  		$("footer").html("<p>\"It doesn't matter if the glass is half empty or half full; all that matters is you're the one pouring the water.\" - Mark Cuban | Long Tran &copy; 2015");
 	}
-
 	$(".introText").delay(1500).fadeOut(700);
 	$(".introView").delay(1800).slideUp('slow', function() {
-		$("introView").show();
-		$(".headerCustom").fadeIn(700, function() {
-			//$("#portfolio").fadeIn();
-			$(".container").fadeIn(1000, function() {
+		$("introView").slideDown();
+		$(".headerCustom").slideDown('slow', function() {
+			$(".container").slideDown('slow', function() {
 				$("footer").fadeIn();		
 			});
 		});
 	});
 	$("#eventButton").click(function() {
-		$('html,body').animate({scrollTop: $(this).offset().top - 30}, 300);
-		
-		console.log($(this).height());
+		$('html,body').animate({scrollTop: $(this).offset().top - 30}, 200);
 		$("#portfolio").fadeOut(300);
-		$("#events").fadeIn(300);		
+		$("#resume").fadeOut(300);		
+		$("#events").fadeIn(300);
 	});
 	$("#projButton").click(function() {
-		$('html,body').animate({scrollTop: $(this).offset().top - 30}, 300);
-		console.log($(this).height());
+		$('html,body').animate({scrollTop: $(this).offset().top - 30}, 200);
 		$("#events").fadeOut(300);		
+		$("#resume").fadeOut(300);
 		$("#portfolio").fadeIn(300);
+	});
+	$("#resumeButton").click(function() {
+		$('html,body').animate({scrollTop: $(this).offset().top - 30}, 200);
+		$("#events").fadeOut(300);		
+		$("#portfolio").fadeOut(300);
+		$("#resume").fadeIn(300);
 	});
 }
