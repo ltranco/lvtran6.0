ce484ec828ec67ed78b319d0670433505aa70da1
Long Tran <ltran@Longs-MacBook-Pro.local>
2015-08-20 12:37:08 -0700

    got sticky control center

--- js/init.js
+++ js/init.js
@@ -1,4 +1,6 @@
 function initLVT() {
+	var keepThisOffset = -1;
+
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  		$("footer").html("<p>\"It doesn't matter if the glass is half empty or half full; all that matters is you're the one pouring the water.\" - Mark Cuban | Long Tran &copy; 2015");
 	}
@@ -12,21 +14,38 @@ function initLVT() {
 		});
 	});
 	$("#eventButton").click(function() {
-		$('html,body').animate({scrollTop: $(this).offset().top - 30}, 200);
+		if(keepThisOffset == -1) {
+			keepThisOffset = $(this).offset().top - 30;
+			$('html,body').animate({scrollTop: keepThisOffset}, 200);
+		}
 		$("#portfolio").fadeOut(300);
 		$("#resume").fadeOut(300);		
 		$("#events").fadeIn(300);
 	});
 	$("#projButton").click(function() {
-		$('html,body').animate({scrollTop: $(this).offset().top - 30}, 200);
+		if(keepThisOffset == -1) {
+			keepThisOffset = $(this).offset().top - 30;
+			$('html,body').animate({scrollTop: keepThisOffset}, 200);
+		}
 		$("#events").fadeOut(300);		
 		$("#resume").fadeOut(300);
 		$("#portfolio").fadeIn(300);
 	});
 	$("#resumeButton").click(function() {
-		$('html,body').animate({scrollTop: $(this).offset().top - 30}, 200);
+		if(keepThisOffset == -1) {
+			keepThisOffset = $(this).offset().top - 30;
+			$('html,body').animate({scrollTop: keepThisOffset}, 200);
+		}
 		$("#events").fadeOut(300);		
 		$("#portfolio").fadeOut(300);
 		$("#resume").fadeIn(300);
 	});
+
+	var $window = $(window), $stickyEl = $('.control'), elTop = $stickyEl.offset().top, inTop = $("#intro").offset().top;
+    $window.scroll(function() {
+    	if($window.scrollTop() == 0) {
+    		keepThisOffset = -1;
+    	}
+        $stickyEl.toggleClass('stickyControl', $window.scrollTop() > elTop);
+    });
 }
