1956506f79215688f91cc2147a5a74569e30a577
Long Tran <ltran@Longs-MacBook-Pro.local>
2015-08-20 17:44:46 -0700

    disable tooltip on mobile

--- js/init.js
+++ js/init.js
@@ -3,6 +3,9 @@ function initLVT() {
 
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  		$("footer").html("<p>\"It doesn't matter if the glass is half empty or half full; all that matters is you're the one pouring the water.\" - Mark Cuban | Long Tran &copy; 2015");
+ 		$("#resumeButton").removeClass("tooltipped");
+ 		$("#eventButton").removeClass("tooltipped");
+ 		$("#portfolioButton").removeClass("tooltipped");
 	}
 	$(".introText").delay(1500).fadeOut(700);
 	$(".introView").delay(1800).slideUp('fast', function() {
