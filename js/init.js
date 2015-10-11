af4ffbc0a94f8d434903811af1e382ac54bea61c
Long Tran <lt@ucsd.edu>
2015-10-11 01:40:12 -0600

    push name to newline in footer on mobile

--- js/init.js
+++ js/init.js
@@ -17,7 +17,7 @@ function initLVT() {
 	else window.addEventListener('load', cb);
 
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- 		$("footer").html("<p>\"It doesn't matter if the glass is half empty or half full; all that matters is you're the one pouring the water.\" - Mark Cuban | Long Tran &copy; 2015");
+ 		$("footer").html("<p>\"It doesn't matter if the glass is half empty or half full; all that matters is you're the one pouring the water.\" - Mark Cuban<br>Long Tran &copy; 2015");
  		$(".control").html('<a id="resumeButton" class="btn-floating btn-small"><i class="material-icons">assignment</i></a><a id="portfolioButton" class="btn-floating btn-small"><i class="material-icons">code</i></a><a id="eventsButton" class="btn-floating btn-small"><i class="material-icons">flight_takeoff</i></a>');
 	}
 	$(".introText").delay(1500).fadeOut(700);
