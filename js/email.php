405c9f446e28f6fd57a5f99b4f2fe183ae4be715
Long Tran <v.long128@gmail.com>
2015-11-14 02:55:31 -0800

    revamping freelance section

--- js/email.php
+++ js/email.php
@@ -6,5 +6,5 @@
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-    mail($email, "Sample HTML Email", "<h1>TEST</h1>", $headers);
+    mail($email, "Sample HTML Email", $message, $headers);
 ?>
