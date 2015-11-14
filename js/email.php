30676df086155d4b3214c0f04a9d0314f26c9a48
Long Tran <v.long128@gmail.com>
2015-11-14 02:48:39 -0800

    revamping freelance section

--- js/email.php
+++ js/email.php
@@ -1,4 +1,8 @@
 <?php 
     $email = $_POST["email"];
-    mail($email, "Sample HTML Email", "<h1>TEST</h1>");
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    mail($email, "Sample HTML Email", "<h1>TEST</h1>", $headers);
 ?>
